fc3809647a33edb1d2d1d426110097334c1bf18c
vine96 <viniciuspecci@hotmail.com>
2024-06-18 17:24:44 -0300

    First commit xD

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <p>Meu primeiro projeto xD</p>
+    </div>
+  );
+}
+
+export default App;
