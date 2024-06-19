8145116f8ec90d4c40ea3ae93dbe85283f880428
vine96 <viniciuspecci@hotmail.com>
2024-06-19 10:05:22 -0300

    Pequenas alterações iniciais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Vinicius'
+  function sum(a, b){
+    return a + b
+  }
+  const url = 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/04/21/pc-gamer-thumb-ts56rack6f6u.jpg'
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
-      <p>Meu primeiro projeto xD</p>
+      <p>Meu primeiro projeto xD by: {name.toUpperCase()}</p>
+      <p>Soma: {sum(1, 3)}</p>
+      <img src={url} alt="Imagem de computador gamer" />
     </div>
   );
 }
